d3052473b40b740c8cf3334ca8e1fb846d318df9
m3il <96340376+m3il@users.noreply.github.com>
2023-03-09 11:48:15 +0000

    Create Credit.java

---
+++ Credit.java
@@ -0,0 +1,20 @@
+import java.util.Scanner
+
+public class Credit{
+  public static main void(String[] args){
+    Scanner in = new Scanner(System.in);
+    
+    int maxCredit;
+    int usedCredit;
+    
+    System.out.println("Enter the maximum amount of available credit: ");
+    maxCredit = in.nextInt();
+    System.out.println("Enter the amount of used credit: ");
+    usedCredit = in.nextInt;
+    
+    int availableCredit = maxCredit - usedCredit;
+    
+    System.out.println("Your current available credit is:" + availableCredit);
+    
+  }
+}
